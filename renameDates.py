ddc03c19de94a09ff2971654d93857aa3a9910df
Steven Bruno <sbruno636@gmail.com>
2018-09-26 13:35:31 -0500

    wrote renaming script

--- renameDates.py
+++ renameDates.py
@@ -14,3 +14,23 @@ datePattern = re.compile(r"""^(.*?)
                         (.*?)$
                         """, re.VERBOSE)
 
+for amerFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
+
+    if mo == None:
+        continue
+    
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
+
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+    
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+    print('Renaming "%s" to "%s" ...' % (amerFilename, euroFilename))
+    #shutil.move(amerFilename, euroFilename)
