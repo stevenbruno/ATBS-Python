742e87c260ef759b5f7db365beea688ceb2eeb91
Steven Bruno <sbruno636@gmail.com>
2018-10-02 11:51:38 -0500

    create countyData dict, write to census2010.py

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -16,3 +16,18 @@ for row in range(2, sheet.max_row + 1):
     county = sheet['C' + str(row)].value
     pop = sheet['D' + str(row)].value
 
+    countyData.setdefault(state, {})
+    countyData[state].setdefault(county, {'tracts':0, 'pop': 0})
+
+    countyData[state][county]['tracts'] += 1
+    countyData[state][county]['pop'] += int(pop)
+
+
+print('Writing results...')
+resultFile = open('census2010.py', 'w')
+resultFile.write('allData = ' + pprint.pformat(countyData))
+resultFile.close()
+print('Done')
+
+
+
